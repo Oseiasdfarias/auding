25990121e01af6154df4f8fccab0fd6f2ed52d6a
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-03-09 02:08:44 -0300

    repositorio atualizado

--- audig/main.py
+++ audig/main.py
@@ -2,14 +2,20 @@
 from paths import Caminhos
 from mixer import TocarAudios
 
-# Diretório raiz do curso.
-path = "/home/bits/CursoIngles/Curso_de_Ingles_vm5_2022/01_Fundacao"
-# Subdiretórios a serem listado os áudios
-sub_dirs = ["Módulo 02", ]
-# Instanciando um objeto Caminhos
-caminhos = Caminhos(path=path, subdirs=sub_dirs)
-# obtendo os caminhos absolutos dos arquivos de áudios.
-paths_abs = caminhos.get_paths()
 
-toca_audios = TocarAudios(paths_abs=paths_abs)
-toca_audios.ouvir_lista()
+def run() -> None:
+    # Diretório raiz do curso.
+    path = "/home/bits/CursoIngles/Curso_de_Ingles_vm5_2022/01_Fundacao"
+    # Subdiretórios a serem listado os áudios
+    sub_dirs = ["Módulo 02", ]
+    # Instanciando um objeto Caminhos
+    caminhos = Caminhos(path=path, subdirs=sub_dirs)
+    # obtendo os caminhos absolutos dos arquivos de áudios.
+    paths_abs = caminhos.get_paths()
+
+    toca_audios = TocarAudios(paths_abs=paths_abs)
+    toca_audios.ouvir_lista()
+
+
+if __name__ == "__main__":
+    run()
